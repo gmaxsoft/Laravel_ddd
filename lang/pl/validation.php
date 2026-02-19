a1a8c2c63ae9ff8459edfffc9a90e99571e09a50
Grzegorz Miśków <133238725+gmaxsoft@users.noreply.github.com>
2026-02-19 14:05:52 +0100

    Korekta walidacji języka Polskiego

---
+++ lang/pl/validation.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    'min' => [
+        'array' => 'Pole :attribute musi zawierać co najmniej :min elementów.',
+        'file' => 'Pole :attribute musi mieć co najmniej :min kilobajtów.',
+        'numeric' => 'Pole :attribute musi być co najmniej :min.',
+        'string' => 'Pole :attribute musi mieć co najmniej :min znaków.',
+    ],
+
+    'password' => [
+        'letters' => 'Hasło musi zawierać co najmniej jedną literę.',
+        'mixed' => 'Hasło musi zawierać co najmniej jedną wielką i jedną małą literę.',
+        'numbers' => 'Hasło musi zawierać co najmniej jedną cyfrę.',
+        'symbols' => 'Hasło musi zawierać co najmniej jeden symbol.',
+        'uncompromised' => 'Podane hasło zostało ujawnione w wycieku danych. Wybierz inne hasło.',
+    ],
+
+    'attributes' => [
+        'password' => 'hasło',
+        'email' => 'adres e-mail',
+        'name' => 'imię',
+    ],
+
+];
